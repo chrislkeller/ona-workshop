c12dd2f3e211ff23dd58fa2af730cda34e2b008e
Chris Keller <christopherlawrencekeller@gmail.com>
2013-10-08 15:17:02 -0700

    initial commit

---
+++ 2-creating-separate-template-files/handlebars-ajax.js
@@ -0,0 +1,21 @@
+var jqueryNoConflict = jQuery;
+
+// render handlebars templates via ajax
+function getTemplateAjax(path, callback) {
+    var source, template;
+    jqueryNoConflict.ajax({
+        url: path,
+        success: function (data) {
+            source = data;
+            template = Handlebars.compile(source);
+            if (callback) callback(template);
+        }
+    });
+};
+
+// function to compile handlebars template
+function renderHandlebarsTemplate(withTemplate,inElement,withData){
+    getTemplateAjax(withTemplate, function(template) {
+        jqueryNoConflict(inElement).html(template(withData));
+    })
+};
